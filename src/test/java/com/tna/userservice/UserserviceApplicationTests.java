be54beb13303f0f2dfa962b6efd46c4e845111fb
Abderrahim <a.aabane@tnaconsulting.net>
2021-07-13 11:41:24 +0100

    Config

--- src/test/java/com/tna/userservice/UserserviceApplicationTests.java
+++ src/test/java/com/tna/userservice/UserserviceApplicationTests.java
@@ -1,22 +1,13 @@
-package com.tna.userservice;
-
-import com.tna.userservice.Services.CustomerService;
-import lombok.extern.log4j.Log4j2;
-import org.joda.time.DateTime;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Date;
-
-@SpringBootTest
-@Log4j2
-class UserserviceApplicationTests {
-	@Autowired
-	CustomerService customerService;
-	@Test
-	void contextLoads() {
-
-	}
-
-}
+//package com.tna.userservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UserserviceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
