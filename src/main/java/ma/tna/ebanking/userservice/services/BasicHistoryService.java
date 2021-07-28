66b3e3b93222bb7c892155011e07d210f740d163
Abderrahim <a.aabane@tnaconsulting.net>
2021-07-28 18:03:54 +0100

    adding changes

--- src/main/java/ma/tna/ebanking/userservice/services/BasicHistoryService.java
+++ src/main/java/ma/tna/ebanking/userservice/services/BasicHistoryService.java
@@ -4,4 +4,4 @@ import ma.tna.audit.model.Auditable;
 import ma.tna.audit.service.HistoryService;
 import org.springframework.stereotype.Service;
 @Service
-public class BasicHistoryService<T extends Auditable<T>> extends HistoryService<T> { }
+public class BasicHistoryService<T extends Auditable<T>> extends HistoryService<T> {}
